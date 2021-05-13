b9363dd4ed56cd9c11ac8b675403a7fc1ae0ecc6
mvslegt <mvslegt@gmail.com>
2021-05-13 16:47:53 +0200

    Lab 10

---
+++ libs/store/shared-ui/src/lib/header/header.component.stories.ts
@@ -0,0 +1,21 @@
+import { text, number, boolean } from '@storybook/addon-knobs';
+import { HeaderComponent } from './header.component';
+//IMPORT TOOLBAR MODULE
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+//IMPORT THEME
+import '@angular/material/prebuilt-themes/deeppurple-amber.css';
+
+export default {
+  title: 'HeaderComponent',
+  component: HeaderComponent,
+};
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: [MatToolbarModule],
+  },
+  props: {
+    title: text('title', ''),
+  },
+});
