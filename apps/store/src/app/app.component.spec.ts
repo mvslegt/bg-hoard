0bb8265d2d1b560f46c32b09665a9655aa278774
mvslegt <mvslegt@gmail.com>
2021-05-13 20:26:29 +0200

    Fixed unit tests

--- apps/store/src/app/app.component.spec.ts
+++ apps/store/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreSharedUiModule } from '@bg-hoard/store/shared-ui';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientModule, StoreSharedUiModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -18,16 +20,18 @@ describe('AppComponent', () => {
 
   it(`should have as title 'store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.componentInstance.title = 'Board Game Hoard';
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('store');
+    expect(app.title).toEqual('Board Game Hoard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.componentInstance.title = 'Board Game Hoard';
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to store!'
+    expect(compiled.querySelector('mat-toolbar').textContent).toContain(
+      'Board Game Hoard'
     );
   });
 });
