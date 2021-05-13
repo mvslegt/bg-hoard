5c3510cebc3992e7e2e2c4e0b90fcce30302a34b
mvslegt <mvslegt@gmail.com>
2021-05-13 19:06:07 +0200

    Lab 13d

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,9 +1,9 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (host: Tree, schema: any) {
+export default async function (host: Tree, schema: Schema) {
   await libraryGenerator(host, {
-    name: 'util-' + schema.name,
+    name: `util-${schema.name}`,
     directory: schema.directory,
     tags: `scope:${schema.directory},type:util`,
   });
@@ -11,4 +11,9 @@ export default async function (host: Tree, schema: any) {
   return () => {
     installPackagesTask(host);
   };
+}
+
+interface Schema {
+  name: string;
+  directory: 'store' | 'api' | 'shared';
 }
