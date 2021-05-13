c72cc50fe35b4feb27c6f5003b2cc58b6be0778b
mvslegt <mvslegt@gmail.com>
2021-05-13 16:53:30 +0200

    Lab11

--- apps/store-shared-ui-e2e/src/integration/header/header.component.spec.ts
+++ apps/store-shared-ui-e2e/src/integration/header/header.component.spec.ts
@@ -1,7 +1,17 @@
 describe('store-shared-ui', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=headercomponent--primary&knob-title='));
+  beforeEach(() =>
+    cy.visit('/iframe.html?id=headercomponent--primary&knob-title=')
+  );
 
   it('should render the component', () => {
     cy.get('bg-hoard-header').should('exist');
+  });
+
+  it('should show the title', () => {
+    cy.visit(
+      '/iframe.html?id=headercomponent--primary&knob-title=BoardGameHoard'
+    );
+
+    cy.get('bg-hoard-header').contains('BoardGameHoard');
   });
 });
